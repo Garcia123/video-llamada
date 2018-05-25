5d2b8ce903e369dd9fcdc852297751e3afbc219c
Jose garcia <newmoon798@gmail.com>
2018-05-25 17:36:03 -0500

    empesar sokect oi

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-var express = require("express");
+var express = require("express.io");
 var app = express();
+app.http().io();
+var PORT = 3000;
 console.log("server started");
-
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
-app.listen(3000);
+app.io.route("ready", (req) => {
+
+})
+
+app.listen(PORT);
