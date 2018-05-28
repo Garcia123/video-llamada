78451e8a48348a057f32b83798bd375c7081012c
Jose garcia <newmoon798@gmail.com>
2018-05-28 17:21:58 -0500

    configurando la senalisacion con RTCconection

--- server.js
+++ server.js
@@ -6,20 +6,32 @@ console.log("server started on port " + PORT);
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+   res.render("index.ejs");
 });
 
 app.io.route("ready", (req) => {
-    req.io.join(req.data);
-    app.io.room(req.data).broadcast("announce", {
-        message: "new client in the " + req.data + "room."
-    })
+
+   req.io.join(req.data.chat_room);
+   req.io.join(req.data.signal_room);
+
+   app.io.room(req.data).broadcast("announce", {
+      message: "new client in the " + req.data + "room."
+   });
+   
 });
 
 app.io.route("send", (req) => {
-    app.io.room(req.data.room).broadcast("message", {
-        message: req.data.message,
-        author: req.data.author
-    });
+   app.io.room(req.data.room).broadcast("message", {
+      message: req.data.message,
+      author: req.data.author
+   });
 });
+
+app.io.route("signal", (req) => {
+   req.io.room(req.data.room).broadcast('signaling_mensaje',{
+      type: req.data.type,
+      message: req.data.message
+   });
+});
+
 app.listen(PORT);
